6933a65f9069fe2660aae5aa7e119775994675e2
vaishali023 <vaishalichaudhary011@gmail.com>
2022-06-09 20:55:39 -0400

    updated script
    made a function to fetch API

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const createBtn = document.getElementById("create");
+const createBtn = document.getElementById("create-btn");
 const quoteGen = document.getElementById("quote");
 
+function findQuotes(e){
+    e.preventDefault();
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    quoteGen.innerHTML
+  });
+}
+
+
+//Event Listener
+createBtn.addEventListener('submit', findQuotes);
