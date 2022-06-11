b039cfb78dca72da7fa2a5a4a0f41277f3970c69
vaishali023 <vaishalichaudhary011@gmail.com>
2022-06-10 22:58:03 -0400

    updated script

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const createBtn = document.getElementById("create-btn");
 const quoteGen = document.getElementById("quote");
 
-function findQuotes(e){
-    e.preventDefault();
+function findQuotes(){
+    // e.preventDefault();
 fetch("https://type.fit/api/quotes")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
     console.log(data);
-    quoteGen.innerHTML
+    // quoteGen.innerHTML = 
   });
 }
 
